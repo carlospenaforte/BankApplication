8f60ee852df1d2144d3d96c60386891c002af7d8
carlospenaforte <carlosedu_1132@hotmail.com>
2025-05-04 14:08:16 -0300

    Model Bank Account - BankAccountModel getting the id of usermodel; usermodel with the basic data from user; transfermodel with the data of the transfer(from,amount,to,datetime,optional description, id)

---
+++ BankApplication/Models/BankAccountModel.cs
@@ -0,0 +1,12 @@
+﻿namespace BankApplication.Models
+{
+    public class BankAccountModel
+    {
+        public int Id_Account { get; set; }
+        public double Amount { get; set; }
+
+
+        public int Id_User { get; set; }
+        public UserModel UserModel { get; set; }
+    }
+}
---
+++ BankApplication/Models/TransfersModel.cs
@@ -0,0 +1,12 @@
+﻿namespace BankApplication.Models
+{
+    public class TransfersModel
+    {
+        public int Id_Transfer { get; set; }
+        public int Id_Account_From { get; set; }
+        public int Id_Account_To { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime TransferDate { get; set; }
+        public string Description { get; set; } = string.Empty;
+    }
+}
