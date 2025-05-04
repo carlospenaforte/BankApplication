6f7abe3ca6dd408b47a22ad0230d3c84f8ff6b81
carlospenaforte <carlosedu_1132@hotmail.com>
2025-05-04 17:19:43 -0300

    Added Extensions Entity Framework Core, SqlServer, Tools;
    Created Folder data and cs file DataContext. Maked the context;
    
    View folder and file index for bank account;
    
    Adding button for bank account page in the layout.

---
+++ BankApplication/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BankApplication.Models;
+
+namespace BankApplication.Data
+{
+    public class DataContext : DbContext 
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<UserModel> Users { get; set; }
+        public DbSet<BankAccountModel> BankAccounts { get; set; }
+        public DbSet<TransfersModel> BankTransactions { get; set; }
+    }
+}
