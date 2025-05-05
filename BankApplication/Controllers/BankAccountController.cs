869803c1d16cd74df2be505de16be5db34018fd3
carlospenaforte <carlosedu_1132@hotmail.com>
2025-05-04 23:17:25 -0300

    FIXED BUGS AND ADDING CONTROLLER BANK ACCOUNT

--- BankApplication/Controllers/BankAccountController.cs
+++ BankApplication/Controllers/BankAccountController.cs
@@ -1,11 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using BankApplication.Models;
+using BankApplication.Data;
 
 namespace BankApplication.Controllers
 {
     public class BankAccountController : Controller
     {
+        private readonly DataContext _context;
+        public BankAccountController(DataContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
+            var account = new BankAccountModel()
+            {
+                Id_Account = 1,
+                Amount = 1000.00,
+                Id_User = 1,
+
+            };
             return View();
         }
     }
--- BankApplication/Models/TransfersModel.cs
+++ BankApplication/Models/TransfersModel.cs
@@ -3,8 +3,13 @@
     public class TransfersModel
     {
         public int Id_Transfer { get; set; }
+
         public int Id_Account_From { get; set; }
+        public BankAccountModel Account_From { get; set; }
+
         public int Id_Account_To { get; set; }
+        public BankAccountModel Account_To { get; set; }
+
         public decimal Amount { get; set; }
         public DateTime TransferDate { get; set; }
         public string Description { get; set; } = string.Empty;
